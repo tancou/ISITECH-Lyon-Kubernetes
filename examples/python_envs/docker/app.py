2a7dfd876dadf658d36e839a3177dc7ee99ac5cf
tancou <git.commits@tancou.me>
2024-11-05 12:36:05 +0100

    Ajout d'un env classic

--- examples/python_envs/docker/app.py
+++ examples/python_envs/docker/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 import os
+from os import environ
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World from py-env!"
+
+@app.route("/secret")
+def secret():
+    SECRET_KEY = environ.get('SECRET_KEY')
+    return "Le secret est : {}".format(SECRET_KEY)
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
